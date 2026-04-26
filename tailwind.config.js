3a7985140df5e70568a1d98aac666beecb410230
agimharizaj <143949144+agimharizaj@users.noreply.github.com>
2026-04-27 00:42:17 +0100

    fix: downgrade to Tailwind v3, fix content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {},
